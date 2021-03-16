2115fb9a858c794e19cbd16c48e7e8a8ff7d26f2
Evgeny Brega <eabrega@gmail.com>
2021-03-16 09:30:57 +0300

    fixed

--- src/store/fielad.ts
+++ src/store/fielad.ts
@@ -6,8 +6,8 @@ export interface IField {
 export class Field {
     public Id: string;
     public Name: String;
-    public Area = 6767;
-    public Code = 9823;
+    public Area = 767;
+    public Code = 92823;
 
     constructor(ifield: IField) {
         this.Id = ifield.id;
